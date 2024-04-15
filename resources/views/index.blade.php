27a3ba773cc0dabd26c9cdef6c30827d389c055b
JamesDDando <jdd6763@students.uc-bcf.edu.ph>
2024-04-16 01:58:42 +0800

    Sixth Commit (Header and Footer Created)

---
+++ resources/views/index.blade.php
@@ -0,0 +1,28 @@
+@include('partials.header')
+
+<div class="container mx-auto">
+    <table class="table-auto w-full">
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Description</th>
+                <th>ISBN</th>
+                <th>Date Published</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($books as $book)
+                <tr>
+                    <td class="border px-4 py-2">{{ $book->title }}</td>
+                    <td class="border px-4 py-2">{{ $book->author }}</td>
+                    <td class="border px-4 py-2">{{ $book->description }}</td>
+                    <td class="border px-4 py-2">{{ $book->isbn }}</td>
+                    <td class="border px-4 py-2">{{ $book->date_published }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
+@include('partials.footer')
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-blue-500 py-4 mt-8">
+    <div class="container mx-auto text-white text-center">
+        <p>&copy; {{ date('Y') }} Books App. All rights reserved.</p>
+    </div>
+</footer>
