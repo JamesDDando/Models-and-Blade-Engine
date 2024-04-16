0b88cdc653359dbc33530f6d533129a3eef87801
JamesDDando <jdd6763@students.uc-bcf.edu.ph>
2024-04-16 20:43:48 +0800

    Final Commit

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,30 +1,36 @@
 @include('partials.header')
 
-
-<div class="container mx-auto">
-    <table class="table-auto w-full">
-        <thead>
-            <tr>
-                <th>Title</th>
-                <th>Author</th>
-                <th>Description</th>
-                <th>ISBN</th>
-                <th>Date Published</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($books as $book)
-                <tr>
-                    <td class="border px-4 py-2">{{ $book->title }}</td>
-                    <td class="border px-4 py-2">{{ $book->author }}</td>
-                    <td class="border px-4 py-2">{{ $book->description }}</td>
-                    <td class="border px-4 py-2">{{ $book->isbn }}</td>
-                    <td class="border px-4 py-2">{{ $book->date_published }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
-
+<div class="flex flex-col">
+    <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+        <div class="overflow-hidden">
+          <table
+            class="min-w-full text-center text-sm font-light text-surface dark:text-white">
+            <thead
+              class="border-b border-neutral-200 font-medium dark:border-white/10">
+              <tr>
+                <th scope="col" class="px-6 py-4">Title</th>
+                <th scope="col" class="px-6 py-4">Author</th>
+                <th scope="col" class="px-6 py-4">Description</th>
+                <th scope="col" class="px-6 py-4">ISBN</th>
+                <th scope="col" class="px-6 py-4">Date Published</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($books as $book)
+              <tr class="border-b border-neutral-200 dark:border-white/10">
+                <td class="whitespace-nowrap px-6 py-4">{{ $book->title }}</td>
+                <td class="whitespace-nowrap px-6 py-4">{{ $book->author }}</td>
+                <td class="whitespace-nowrap px-6 py-4">{{ $book->description }}</td>
+                <td class="whitespace-nowrap px-6 py-4">{{ $book->isbn }}</td>
+                <td class="whitespace-nowrap px-6 py-4">{{ $book->date_published }}</td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
 
 @include('partials.footer')
